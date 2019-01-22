a250b778f70f45014cf592e93b320d989dbc3d39
Suricatto <samuel@technopartner.com.br>
2019-01-22 10:54:59 -0200

    Rest test

--- tp_workers/socks_proxy/src/socks_proxy.py
+++ tp_workers/socks_proxy/src/socks_proxy.py
@@ -3,9 +3,15 @@ import urllib2
 import socket
 import socks
 import time
+import requests
+import json
+import numpy as np
 #import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
 from multiprocessing import Process
+
+def get_random_id():
+    return np.random.randint(2147483647, 9223372036854775807, size=1, dtype=np.int64)[0]
 
 def initial_state():
     pass
@@ -24,14 +30,6 @@ def nap_processors(host, user, password, topic):
     start_time = time.time()
 
     try:
-        #client = mqtt.Client("P1") #create new instance
-
-        #client.username_pw_set(user, password)
-        
-        #client.connect(host) #connect to broker
-
-        #client.publish(topic,"OFF")#publish
-
         publish.single(topic, "payload", hostname=host, auth={'username': user, 'password': password})
         mzbench.notify(('success_requests', 'counter'), 1)
         
@@ -57,6 +55,57 @@ def socks_processors(host):
     except Exception as error:
         mzbench.notify(('failed_requests', 'counter'), 1)
         print "Timeout: {0}".format(str(error))
+
+
+def rest_processors(host, headers):
+    start_time = time.time()
+    item = { #UPDATE ANTIGO
+          "antenna": get_random_id(),
+          "manufacturer": "SAMSUNG",
+          "model": "J7MOCK",
+          "product": "SMART",
+          "device": "J7MOCK",
+          "hardware": "LALALA",
+          "brand": "SAMSUNG",
+          "display": "DUNO",
+          "host": "DUNO",
+          "board": "GREAT",
+          "sdk_version": "1.2.8-fake",
+          "android_sdk_version": 19
+    }
+
+    item = { #UPDATE NOVO
+        "id": get_random_id(),
+        "tpsdk": "10.0.1",
+        "asdk": 24
+    }
+
+    try:
+        item = json.dumps(item, encoding='utf8')
+
+        r = requests.post(host, data=item, headers=headers, timeout=5)
+
+        if r.status_code not in (200, 201):
+            print "Failed {0}".format(r.status_code)
+            mzbench.notify(('failed_requests', 'counter'), 1)
+        else:
+            mzbench.notify(('success_requests', 'counter'), 1)
+
+        mzbench.notify(('request_time', 'histogram'), (time.time() - start_time))
+    except Exception as error:
+        mzbench.notify(('failed_requests', 'counter'), 1)
+        print "Timeout: {0}".format(str(error))
+
+
+def rest_load(host, authorization_token):
+    headers = { 
+        'Content-Type': 'application/json',
+        'x-api-key': authorization_token
+    }
+    
+    process = Process(target=rest_processors, args=[host, headers])
+    process.start()
+    process.join()
 
 
 def socks_load(host, proxy):
